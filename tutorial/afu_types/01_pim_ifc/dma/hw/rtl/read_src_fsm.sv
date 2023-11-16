b9c1de9f4400f1be96deacc6e8dc552724ada21a
Murphy, Peter J <peter.j.murphy@intel.com>
2023-11-16 13:42:01 -0800

    adding dispatcher FSMs

---
+++ tutorial/afu_types/01_pim_ifc/dma/hw/rtl/read_src_fsm.sv
@@ -0,0 +1,99 @@
+// Copyright (C) 2022 Intel Corporation
+// SPDX-License-Identifier: MIT
+
+`include "ofs_plat_if.vh"
+
+
+module read_src_fsm #(
+   parameter DATA_W = 512,
+   parameter MODE = dma_pkg::DDR_TO_HOST
+)(
+   input logic clk,
+   input logic reset_n,
+   input logic wr_fsm_done,
+   input  dma_pkg::t_control control,
+   output logic descriptor_fifo_rdack,
+   ofs_plat_axi_mem_if.to_sink src_mem,
+   dma_fifo_if.wr_out  wr_fifo_if
+);
+
+   `define NUM_STATES 4
+
+   enum {
+      IDLE_BIT,
+      ADDR_SETUP_BIT,
+      CP_RSP_TO_FIFO_BIT,
+      WAIT_FOR_WR_RSP_BIT
+   } index;
+
+   enum logic [`NUM_STATES-1:0] {
+      IDLE                = `NUM_STATES'b1<<IDLE_BIT,
+      ADDR_SETUP          = `NUM_STATES'b1<<ADDR_SETUP_BIT,
+      CP_RSP_TO_FIFO      = `NUM_STATES'b1<<CP_RSP_TO_FIFO_BIT,
+      WAIT_FOR_WR_RSP     = `NUM_STATES'b1<<WAIT_FOR_WR_RSP_BIT,
+      XXX = 'x
+   } state, next;
+   
+   assign src_mem.rready = state[CP_RSP_TO_FIFO_BIT] ? wr_fifo_if.not_full : 0;
+
+   always_ff @(posedge clk) begin
+      if (!reset_n) state <= IDLE;
+      else          state <= next;
+   end
+
+   always_comb begin
+      next = XXX;
+      unique case (1'b1)
+         state[IDLE_BIT]: 
+            if ((control.mode == MODE) & (control.descriptor.control == 1)) next = ADDR_SETUP; 
+
+         state[ADDR_SETUP_BIT]:
+            if (src_mem.arvalid & src_mem.arready) next = CP_RSP_TO_FIFO;
+
+         state[CP_RSP_TO_FIFO_BIT]:
+            if (src_mem.rvalid & src_mem.rready & src_mem.rlast) next = WAIT_FOR_WR_RSP;
+
+         state[WAIT_FOR_WR_RSP_BIT]:
+            if (wr_fsm_done) next = IDLE;
+
+      endcase
+
+   end
+
+
+  always_ff @(posedge clk) begin
+     if (!reset_n) begin
+        src_mem.arvalid       <= 1'b0;
+        src_mem.rvalid        <= 1'b0;
+        src_mem.wvalid        <= 1'b0;
+        src_mem.awvalid       <= 1'b0;
+        descriptor_fifo_rdack <= 1'b0;
+     end else begin
+        unique case (1'b1)
+           state[IDLE_BIT]: begin
+              src_mem.arvalid <= 1'b0;
+              descriptor_fifo_rdack <= 1'b0;
+           end 
+           
+           state[ADDR_SETUP_BIT]: begin
+               src_mem.arvalid <= 1'b1;
+               src_mem.araddr  <= control.descriptor.src_addr;
+               src_mem.arlen   <= control.descriptor.length;
+               src_mem.arburst <= control.descriptor.burst;
+               src_mem.arsize  <= control.descriptor.size;
+           end
+           
+           state[CP_RSP_TO_FIFO_BIT]: begin
+               src_mem.arvalid <= 1'b0;
+               wr_fifo_if.wr_data <= src_mem.r.data;
+               wr_fifo_if.wr_en   <= !wr_fifo_if.not_full & src_mem.rvalid;
+           end
+           
+           state[WAIT_FOR_WR_RSP_BIT]:
+              if (wr_fsm_done) descriptor_fifo_rdack <= 1'b1;
+          
+       endcase
+     end
+  end
+
+endmodule
---
+++ tutorial/afu_types/01_pim_ifc/dma/hw/rtl/write_dest_fsm.sv
@@ -0,0 +1,18 @@
+// Copyright (C) 2022 Intel Corporation
+// SPDX-License-Identifier: MIT
+
+`include "ofs_plat_if.vh"
+
+
+module write_dest_fsm #(
+   parameter DATA_W = 512
+)(
+   input logic clk,
+   input logic reset_n,
+   output logic wr_fsm_done,
+   input dma_pkg::t_descriptor descriptor,
+   ofs_plat_axi_mem_if.to_src dest_mem,
+   dma_fifo_if.wr_out  rd_fifo_if
+);
+
+endmodule
