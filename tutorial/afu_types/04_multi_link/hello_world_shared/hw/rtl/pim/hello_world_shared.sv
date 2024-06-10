964b983e81b559f0d9440b8f0cb1be0bc514932d
Michael Adler <michael.adler@intel.com>
2024-06-10 15:49:54 -0400

    Add multi-link example with shared CSRs

---
+++ tutorial/afu_types/04_multi_link/hello_world_shared/hw/rtl/pim/hello_world_shared.sv
@@ -0,0 +1,356 @@
+// Copyright (C) 2024 Intel Corporation
+// SPDX-License-Identifier: MIT
+
+`include "ofs_plat_if.vh"
+`include "afu_json_info.vh"
+
+//
+// AXI-MM version of hello world AFU example using a shared CSR space and
+// multiple host memory links.
+//
+
+module hello_world_shared
+  #(
+    parameter NUM_CHILDREN = 0
+    )
+   (
+    // CSR interface (MMIO on the host)
+    ofs_plat_axi_mem_lite_if.to_source mmio64_to_afu,
+
+    // Host memory (DMA)
+    ofs_plat_axi_mem_if.to_sink host_mem[1 + NUM_CHILDREN]
+    );
+
+    // Assume that all interfaces share the same clock
+    logic clk;
+    assign clk = host_mem[0].clk;
+    logic reset_n;
+    assign reset_n = host_mem[0].reset_n;
+
+    localparam DATA_WIDTH = host_mem[0].DATA_WIDTH;
+
+    // =========================================================================
+    //
+    //   CSR (MMIO) handling with AXI lite.
+    //
+    // =========================================================================
+
+    //
+    // The primary DFH with the AFU ID and the parent/child info is handled
+    // already, outside this module. Only the CSR space to which the v1 DFH
+    // points has to be managed here.
+    //
+
+    //
+    // The AXI lite interface is defined in
+    // $OPAE_PLATFORM_ROOT/hw/lib/build/platform/ofs_plat_if/rtl/base_ifcs/axi/ofs_plat_axi_mem_lite_if.sv.
+    // It contains fields defined by the AXI standard, though organized
+    // slightly unusually. Instead of being a flat data structure, the
+    // payload for each bus is a struct. The name of the AXI field is the
+    // concatenation of the struct instance and field. E.g., AWADDR is
+    // aw.addr. The use of structs makes it easier to bulk copy or bulk
+    // initialize the full payload of a bus.
+    //
+
+    // Use a copy of the MMIO interface as registers.
+    ofs_plat_axi_mem_lite_if
+      #(
+        // PIM-provided macro to replicate identically sized instances of an
+        // AXI lite interface.
+        `OFS_PLAT_AXI_MEM_LITE_IF_REPLICATE_PARAMS(mmio64_to_afu)
+        )
+      mmio64_reg();
+
+    // Is a CSR read request active this cycle? The test is simple because
+    // the mmio64_reg.arvalid can only be set when the read response buffer
+    // is empty.
+    logic is_csr_read;
+    assign is_csr_read = mmio64_reg.arvalid;
+
+    // Is a CSR write request active this cycle?
+    logic is_csr_write;
+    assign is_csr_write = mmio64_reg.awvalid && mmio64_reg.wvalid;
+
+
+    //
+    // Receive MMIO read requests
+    //
+
+    // Ready for new request iff read request and response registers are empty
+    assign mmio64_to_afu.arready = !mmio64_reg.arvalid && !mmio64_reg.rvalid;
+
+    always_ff @(posedge clk)
+    begin
+        if (is_csr_read)
+        begin
+            // Current read request was handled
+            mmio64_reg.arvalid <= 1'b0;
+        end
+        else if (mmio64_to_afu.arvalid && mmio64_to_afu.arready)
+        begin
+            // Receive new read request
+            mmio64_reg.arvalid <= 1'b1;
+            mmio64_reg.ar <= mmio64_to_afu.ar;
+        end
+
+        if (!reset_n) begin
+            mmio64_reg.arvalid <= 1'b0;
+        end
+    end
+
+    //
+    // Completion data. The AFU's primary feature, including the AFU ID,
+    // is implemented outside this module by ofs_fim_pcie_multi_link_afu_dfh().
+    // MMIO reads here are to addresses outside that feature.
+    //
+    assign mmio64_to_afu.rvalid = mmio64_reg.rvalid;
+    assign mmio64_to_afu.r = mmio64_reg.r;
+
+    always_ff @(posedge clk)
+    begin
+        if (is_csr_read)
+        begin
+            // New read response
+            mmio64_reg.rvalid <= 1'b1;
+
+            mmio64_reg.r <= '0;
+            // The unique transaction ID matches responses to requests
+            mmio64_reg.r.id <= mmio64_reg.ar.id;
+            // Return user flags from request
+            mmio64_reg.r.user <= mmio64_reg.ar.user;
+
+            // AXI addresses are always in byte address space. Ignore the
+            // low 3 bits to index 64 bit CSRs.
+            // Check only a few address bits to simplify the decoder.
+            // There will be multiple address aliases within the full MMIO space.
+            case (mmio64_reg.ar.addr[5:3])
+              // Parents: number of children.
+              // Children: 0
+              0: mmio64_reg.r.data <= NUM_CHILDREN;
+
+              // Data bus width (bytes)
+              1: mmio64_reg.r.data <= DATA_WIDTH / 8;
+
+              default: mmio64_reg.r.data <= '0;
+            endcase
+        end
+        else if (mmio64_to_afu.rready)
+        begin
+            // If a read response was pending it completed
+            mmio64_reg.rvalid <= 1'b0;
+        end
+
+        if (!reset_n)
+        begin
+            mmio64_reg.rvalid <= 1'b0;
+        end
+    end
+
+
+    //
+    // CSR write handling.  Host software must tell the AFU the memory address
+    // to which it should be writing.  The address is set by writing a CSR.
+    //
+
+    // Ready for new request iff write request register is empty
+    assign mmio64_to_afu.awready = !mmio64_reg.awvalid && !mmio64_reg.bvalid;
+    assign mmio64_to_afu.wready  = !mmio64_reg.wvalid && !mmio64_reg.bvalid;
+
+    always_ff @(posedge clk)
+    begin
+        if (is_csr_write)
+        begin
+            // Current write request was handled
+            mmio64_reg.awvalid <= 1'b0;
+            mmio64_reg.wvalid <= 1'b0;
+        end
+        else
+        begin
+            // Receive new write address
+            if (mmio64_to_afu.awvalid && mmio64_to_afu.awready)
+            begin
+                mmio64_reg.awvalid <= 1'b1;
+                mmio64_reg.aw <= mmio64_to_afu.aw;
+            end
+
+            // Receive new write data
+            if (mmio64_to_afu.wvalid && mmio64_to_afu.wready)
+            begin
+                mmio64_reg.wvalid <= 1'b1;
+                mmio64_reg.w <= mmio64_to_afu.w;
+            end
+        end
+
+        if (!reset_n)
+        begin
+            mmio64_reg.awvalid <= 1'b0;
+            mmio64_reg.wvalid <= 1'b0;
+        end
+    end
+
+    // Write response
+    assign mmio64_to_afu.bvalid = mmio64_reg.bvalid;
+    assign mmio64_to_afu.b = mmio64_reg.b;
+
+    always_ff @(posedge clk)
+    begin
+        if (is_csr_write)
+        begin
+            // New write response
+            mmio64_reg.bvalid <= 1'b1;
+
+            mmio64_reg.b <= '0;
+            mmio64_reg.b.id <= mmio64_reg.aw.id;
+            mmio64_reg.b.user <= mmio64_reg.aw.user;
+        end
+        else if (mmio64_to_afu.bready)
+        begin
+            // If a write response was pending it completed
+            mmio64_reg.bvalid <= 1'b0;
+        end
+
+        if (!reset_n)
+        begin
+            mmio64_reg.bvalid <= 1'b0;
+        end
+    end
+
+    // We use MMIO address 0 to set the memory address.  The read and
+    // write MMIO spaces are logically separate so we are free to use
+    // whatever we like.  This may not be good practice for cleanly
+    // organizing the MMIO address space, but it is legal.
+    logic is_mem_addr_csr_write;
+    assign is_mem_addr_csr_write = is_csr_write && (mmio64_reg.aw.addr == '0);
+
+    // DMA address to which this AFU will write.
+    localparam MEM_ADDR_WIDTH = host_mem[0].ADDR_WIDTH;
+    typedef logic [MEM_ADDR_WIDTH-1 : 0] t_mem_addr;
+    t_mem_addr mem_addr;
+
+    // Index into the vector of parent/child host memory interfaces
+    typedef logic [$clog2(1 + NUM_CHILDREN)-1 : 0] t_host_mem_ifc_idx;
+    t_host_mem_ifc_idx ifc_idx;
+
+    always_ff @(posedge clk)
+    begin
+        if (is_mem_addr_csr_write)
+        begin
+            // The host passed in a line address. AXI-MM wants byte-level.
+            mem_addr <= t_mem_addr'({ mmio64_reg.w.data, 6'b0 });
+
+            // The index of the host memory interface to use for the write
+            // is passed in the high 4 bits of the mem_addr command.
+            ifc_idx <= t_host_mem_ifc_idx'(mmio64_reg.w.data[63:60]);
+        end
+    end
+
+
+    // =========================================================================
+    //
+    //   Main AFU logic
+    //
+    // =========================================================================
+
+    //
+    // States in our simple example. In an AFU where performance matters
+    // we would write to both data and address buses in parallel. For
+    // simplicity here we will write to them in different states.
+    //
+    typedef enum logic [1:0]
+    {
+        STATE_IDLE,
+        STATE_ADDR,
+        STATE_DATA
+    }
+    t_state;
+
+    t_state state;
+
+    //
+    // State machine
+    //
+    always_ff @(posedge clk)
+    begin
+        if (!reset_n)
+        begin
+            state <= STATE_IDLE;
+        end
+        else
+        begin
+            // Trigger the AFU when mem_addr is set above.  (When the CPU
+            // tells us the address to which the FPGA should write a message.)
+            if ((state == STATE_IDLE) && is_mem_addr_csr_write)
+            begin
+                state <= STATE_ADDR;
+                $display("AFU running...");
+            end
+
+            // Sit in STATE_ADDR until the address is written to the DMA
+            // interface.
+            if ((state == STATE_ADDR) && host_mem[0].awready)
+            begin
+                state <= STATE_DATA;
+            end
+
+            // The AFU completes its task by writing a single line.  When
+            // the line is written return to idle.  The write will happen
+            // as long as the request channel is not full.
+            if ((state == STATE_DATA) && host_mem[0].wready)
+            begin
+                state <= STATE_IDLE;
+                $display("AFU done...");
+            end
+        end
+    end
+
+    //
+    // Write address of line when in STATE_ADDR.
+    //
+    for (genvar p = 0; p < 1 + NUM_CHILDREN; p += 1)
+    begin
+        // Clearly this is a very simple example, not structured in a way that
+        // takes advantage of the bandwidth of two links. It demonstrates the
+        // basic plumbing, writing through at most one channel per cycle.
+        // The same address is passed to each channel and only the channel
+        // matching the target interface (ifc_idx) is enabled.
+        always_comb
+        begin
+            host_mem[p].awvalid = (state == STATE_ADDR) && (t_host_mem_ifc_idx'(p) == ifc_idx);
+            host_mem[p].aw = '0;
+            host_mem[p].aw.addr = mem_addr;
+            host_mem[p].aw.size = 3'b110;	// 64 bytes
+        end
+    end
+
+    //
+    // Write "Hello world (<ID>)" to memory when in STATE_DATA.
+    //
+    for (genvar p = 0; p < 1 + NUM_CHILDREN; p += 1)
+    begin
+        // Like AW above, this demonstrates basic plumbing without optimization.
+        // Data is passed to each interface but only the active one is marked
+        // valid.
+        always_comb
+        begin
+            host_mem[p].wvalid = (state == STATE_DATA) && (t_host_mem_ifc_idx'(p) == ifc_idx);
+            host_mem[p].w = '0;
+            // "Hello world (<ID>)", ASCII encoded and backwards. The interface
+            // index is embedded in the message so the output will be different
+            // through each parent/child channel.
+            host_mem[p].w.data = { '0, 16'h0029, 8'('h30 + p), 104'h2820646c726f77206f6c6c6548 };
+            host_mem[p].w.strb = ~64'b0;	// Byte mask (enable all)
+        end
+    end
+
+
+    //
+    // This AFU never makes a read request and ignores write responses.
+    //
+    for (genvar p = 0; p < 1 + NUM_CHILDREN; p = p + 1)
+    begin : t
+        assign host_mem[p].arvalid = 1'b0;
+        assign host_mem[p].rready = 1'b1;
+        assign host_mem[p].bready = 1'b1;
+    end
+
+endmodule
---
+++ tutorial/afu_types/04_multi_link/hello_world_shared/sw/hello_world_shared.c
@@ -0,0 +1,193 @@
+// Copyright (C) 2022 Intel Corporation
+// SPDX-License-Identifier: MIT
+
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+#include <uuid/uuid.h>
+
+#include <opae/fpga.h>
+
+// State from the AFU's JSON file, extracted using OPAE's afu_json_mgr script
+#include "afu_json_info.h"
+
+#define CACHELINE_BYTES 64
+#define CL(x) ((x) * CACHELINE_BYTES)
+
+
+//
+// Connect to an accelerator matching UUID
+//
+static fpga_result
+connect_to_matching_accel(const char *accel_uuid,
+                          fpga_handle *accel_handle,
+                          bool *is_ase_sim)
+{
+    fpga_properties filter = NULL;
+    fpga_guid guid;
+    fpga_token accel_token;
+    uint32_t num_matches;
+    fpga_result r;
+
+    assert(accel_handle);
+
+    // Don't print verbose messages in ASE by default
+    setenv("ASE_LOG", "0", 0);
+    *is_ase_sim = false;
+
+    // Set up a filter that will search for an accelerator
+    fpgaGetProperties(NULL, &filter);
+    fpgaPropertiesSetObjectType(filter, FPGA_ACCELERATOR);
+
+    // Add the desired UUID to the filter
+    uuid_parse(accel_uuid, guid);
+    fpgaPropertiesSetGUID(filter, guid);
+
+    // Do the search across the available FPGA contexts
+    r = fpgaEnumerate(&filter, 1, &accel_token, 1, &num_matches);
+    if ((FPGA_OK != r) || (num_matches < 1))
+    {
+        fprintf(stderr, "Accelerator %s not found!\n", accel_uuid);
+        goto out_destroy_filter;
+    }
+
+    // Open accelerator
+    r = fpgaOpen(accel_token, accel_handle, 0);
+    if (FPGA_OK != r)
+    {
+        fprintf(stderr, "Error opening accelerator %s!\n", accel_uuid);
+        goto out_destroy_token;
+    }
+
+    // While the token is available, check whether it is for HW
+    // or for ASE simulation, recording it so probeForASE() below
+    // doesn't have to run through the device list again.
+    fpga_properties accel_props;
+    uint16_t vendor_id, dev_id;
+    fpgaGetProperties(accel_token, &accel_props);
+    fpgaPropertiesGetVendorID(accel_props, &vendor_id);
+    fpgaPropertiesGetDeviceID(accel_props, &dev_id);
+    *is_ase_sim = (vendor_id == 0x8086) && (dev_id == 0xa5e);
+
+  out_destroy_token:
+    fpgaDestroyToken(&accel_token);
+  out_destroy_filter:
+    fpgaDestroyProperties(&filter);
+
+    return r;
+}
+
+
+//
+// Allocate a buffer in I/O memory, shared with the FPGA.
+//
+static volatile void* alloc_buffer(fpga_handle accel_handle,
+                                   ssize_t size,
+                                   uint64_t *wsid,
+                                   uint64_t *io_addr)
+{
+    fpga_result r;
+    volatile void* buf;
+
+    r = fpgaPrepareBuffer(accel_handle, size, (void*)&buf, wsid, 0);
+    if (FPGA_OK != r) return NULL;
+
+    // Get the physical address of the buffer in the accelerator
+    r = fpgaGetIOAddress(accel_handle, *wsid, io_addr);
+    assert(FPGA_OK == r);
+
+    return buf;
+}
+
+
+int main(int argc, char *argv[])
+{
+    fpga_handle accel_handle;
+    volatile char *buf;
+    uint64_t wsid;
+    uint64_t buf_pa;
+    bool is_ase_sim = false;
+    fpga_result r;
+
+    // Find and connect to the accelerators
+    r = connect_to_matching_accel(AFU_ACCEL_UUID, &accel_handle, &is_ase_sim);
+    if (r != FPGA_OK)
+    {
+        fprintf(stderr, "Failure -- exiting\n");
+        exit(1);
+    }
+
+    if (is_ase_sim)
+    {
+        printf("   *** ASE only detects a single AFU (port 0) ***\n");
+    }
+
+    // The DFHv1 spec encodes the offset to the AFU's registers at 0x18
+    // in the primary header.
+    uint64_t csr_base;
+    fpgaReadMMIO64(accel_handle, 0, 0x18, &csr_base);
+    printf("Parent CSRs at 0x%" PRIx64 "\n", csr_base);
+
+    // The AFU returns the number of children at csr_base
+    uint32_t num_children;
+    fpgaReadMMIO32(accel_handle, 0, csr_base, &num_children);
+    printf("Num children: %d\n", num_children);
+
+    // The AFU returns the data bus width at csr_base+8
+    uint64_t bus_width_bytes;
+    fpgaReadMMIO64(accel_handle, 0, csr_base + 0x8, &bus_width_bytes);
+    printf("Bus width is %" PRId64 " bytes\n", bus_width_bytes);
+
+    // Allocate a single page memory buffer. This buffer will be accessible
+    // from the parent and all children at the same address (buf_pa).
+    buf = (volatile char*)alloc_buffer(accel_handle, getpagesize(),
+                                       &wsid, &buf_pa);
+    assert(NULL != buf);
+
+    // Set the low byte of the shared buffer to 0.  The FPGA will write
+    // a non-zero value to it.
+    buf[0] = 0;
+
+    // Tell the accelerator the address of the buffer using cache line
+    // addresses.  The accelerator will respond by writing to the buffer.
+    fpgaWriteMMIO64(accel_handle, 0, 0, buf_pa / CL(1));
+
+    // Spin, waiting for the value in memory to change to something non-zero.
+    while (0 == buf[0])
+    {
+        // A well-behaved program would use _mm_pause(), nanosleep() or
+        // equivalent to save power here.
+    };
+
+    // Print the string written by the FPGA
+    printf("%s\n", buf);
+
+
+    //
+    // Now command writes through the childrens' host memory interfaces.
+    // All commands are written to the same CSR space, managed by the
+    // parent.
+    //
+    for (uint32_t i = 1; i <= num_children; i += 1)
+    {
+        buf[0] = 0;
+        // Write to the CSR space in the parent, commanding it to use
+        // a child host memory channel. The channel index is passed
+        // starting at bit 60 of the address.
+        fpgaWriteMMIO64(accel_handle, 0, 0, ((uint64_t)i << 60) | (buf_pa / CL(1)));
+
+        // Wait for the message written to memory by the FPGA.
+        while (0 == buf[0]) {};
+        printf("%s\n", buf);
+    }
+
+    // Done
+    fpgaReleaseBuffer(accel_handle, wsid);
+    // The children are closed as a side effect of closing the parent handle
+    fpgaClose(accel_handle);
+
+    return 0;
+}
